e0f9cdd24c1dbdd1ceabf9d53ca71f11734d58be
AnthonyB955 <abacidburn576@gmail.com>
2020-06-07 19:20:42 -0400

    Modifief gitignore file and added a file.

---
+++ hungry2.py
@@ -0,0 +1,12 @@
+hungry = input("Are you hungry?")
+if hungry == "yes":
+    print ("Eat Ice Cream")
+    print ("Eat Pizza")
+    print ("Eat Burgers")
+    print ("Eat Fries")
+else:
+    thirsty = input ("Are you thirsty?")
+    if thirsty == "yes":
+            print ("drink water")
+            print ("drink green tea")
+
